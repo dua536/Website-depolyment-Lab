1d59b35990cf8c9c8b5cdae4120e63f19014c56b
DUA <hafsamemon920@gmail.com>
2026-04-24 09:30:59 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Simple message to show JS is working
+console.log("Website loaded successfully!");
+
+// Example: show alert when page loads
+window.onload = function() {
+    console.log("Page fully loaded");
+};
